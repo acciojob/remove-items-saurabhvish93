88d31995d286684da69c189e2b065cd50f0c3774
Saurabh Kumar Vishwakarma <137218388+saurabhvish93@users.noreply.github.com>
2024-09-18 23:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+//your JS code here. If required
+document.getElementById('removebutton').addEventListener('click', () => {
+	const colorSelect = document.getElementById('colorSelect');
+	const selectIndex = colorSelect.selectIndex;
+	if(selectIndex !== -1) {
+		colorSelect.remove(selectIndex);
+	}
+	else {
+		alert('Please Select a color to remove');
+	}
+});
+
